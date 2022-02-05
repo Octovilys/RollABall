21c4eb08dbc10337990102d120cafa719898ba0d
Octovilys <97544346+Octovilys@users.noreply.github.com>
2022-02-05 00:33:19 -0500

    Update PlayerController.cs

--- PlayerController.cs
+++ PlayerController.cs
@@ -8,23 +8,38 @@ public class PlayerController : MonoBehaviour
 {
     public float speed = 0;
     public TextMeshProUGUI countText;
+    public TextMeshProUGUI livesText;
 
     public GameObject winTextObject;
+    public GameObject loseTextObject;
+    public GameObject player;
+    public GameObject livesTextObject;
+
+    public Transform RestartPos;  
 
     private Rigidbody rb;
     private int count;
     private float movementX;
     private float movementY;
+    private int lives;
+
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         count = 0;
+        lives = 3;
+
 
         SetCountText();
         winTextObject.SetActive(false);
+
+        SetLivesText();
+        loseTextObject.SetActive(false);
+
     }
+
 
     void OnMove(InputValue movementValue)
     {
@@ -34,20 +49,41 @@ public class PlayerController : MonoBehaviour
         movementY = movementVector.y;
     }
 
+    void SetLivesText()
+    {
+        livesText.text = "Lives: " + lives.ToString();
+    }
+
     void SetCountText()
     {
         countText.text = "Count: " + count.ToString();
-        if (count >= 12)
+        if (count >= 20)
         {
             winTextObject.SetActive(true);
         }
+        else if (lives == 0)
+        {
+            loseTextObject.SetActive(true);
+        }
     }
+
+    void Update()
+    {
+         if (lives == 0)
+        { 
+        transform.position = RestartPos.position;
+        }
+    }
+    
     void FixedUpdate()
     {
+        
         Vector3 movement = new Vector3(movementX, 0.0f, movementY);
 
         rb.AddForce(movement * speed);
+
     }
+
 
     void OnTriggerEnter(Collider other)
     {
@@ -55,9 +91,24 @@ public class PlayerController : MonoBehaviour
         {
             other.gameObject.SetActive(false);
             count = count + 1;
-
             SetCountText();
         }
+            else if (other.gameObject.CompareTag("Enemy"))
+        {
+            other.gameObject.SetActive(false);
+            lives = lives - 1;
+            SetCountText();
+        }
+        SetLivesText();
 
+            if (count == 12)
+        {
+            transform.position = new Vector3(23.0f, 0.0f, 1.0f);
+        }
     }
+
+   public void TakeDamage(int d)
+   {
+       lives -= d;
+   }  
 }
